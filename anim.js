ca89e337147e6f5e7899e6387388d0076496d4a4
Darko Fotinovski <60361237+Fotinovski@users.noreply.github.com>
2021-04-08 15:18:38 +0200

    test again

--- anim.js
+++ anim.js
@@ -4,14 +4,15 @@ var animation = bodymovin.loadAnimation({
   loop: false,
   autoplay: false,
   // path: 'https://assets7.lottiefiles.com/packages/lf20_qievcjy1.json',
-  path: "./animations/one.json",
+  path: "./animations/home.json",
 });
 
 const btn = document.querySelector(".click");
 var state = "play";
 
 btn.addEventListener("click", () => {
-  animation.goToAndPlay(60, true);
+  animation.setSpeed(.7)
+  animation.goToAndPlay(0, true);
 
   addEventListener("click", () => {
     //   if(state === 'play') {
@@ -38,12 +39,13 @@ var animation1 = bodymovin.loadAnimation({
   renderer: "svg",
   loop: false,
   autoplay: false,
-  path: "https://assets1.lottiefiles.com/temp/lf20_NAo5fW.json",
+  path: './animations/calendar.json',
 });
 
 const btn1 = document.querySelector(".click1");
 
 btn1.addEventListener("click", () => {
+  animation1.setSpeed(.7)
   animation1.goToAndPlay(0, true);
   // animation.pause(0, true)
 });
@@ -76,6 +78,7 @@ var animation3 = bodymovin.loadAnimation({
 const btn3 = document.querySelector(".click3");
 
 btn3.addEventListener("click", () => {
+  animation3.setSpeed(.7)
   animation3.goToAndPlay(0, true);
 });
 
@@ -92,40 +95,41 @@ var animation4 = bodymovin.loadAnimation({
 const btn4 = document.querySelector(".click4");
 
 btn4.addEventListener("click", () => {
+  animation4.setSpeed(.7)
   animation4.goToAndPlay(0, true);
 });
 
 /************/
-var logo = bodymovin.loadAnimation({
-  container: document.getElementById("logo"),
-  renderer: "svg",
-  loop: false,
-  autoplay: true,
-  path: "./animations/lottie.json",
-});
+// var logo = bodymovin.loadAnimation({
+//   container: document.getElementById("logo"),
+//   renderer: "svg",
+//   loop: false,
+//   autoplay: true,
+//   path: "./animations/lottie.json",
+// });
 
-var flamingo = bodymovin.loadAnimation({
-  container: document.getElementById("s1"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "./animations/flamingo.json",
-});
-const s1 = document.querySelector(".s1");
-s1.addEventListener("mouseenter", () => {
-  flamingo.setSpeed(0.3);
-});
-s1.addEventListener("mouseleave", () => {
-  flamingo.setSpeed(1);
-});
+// var flamingo = bodymovin.loadAnimation({
+//   container: document.getElementById("s1"),
+//   renderer: "svg",
+//   loop: true,
+//   autoplay: true,
+//   path: "./animations/flamingo.json",
+// });
+// const s1 = document.querySelector(".s1");
+// s1.addEventListener("mouseenter", () => {
+//   flamingo.setSpeed(0.3);
+// });
+// s1.addEventListener("mouseleave", () => {
+//   flamingo.setSpeed(1);
+// });
 
-var bubble = bodymovin.loadAnimation({
-  container: document.getElementById("s-bg"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "./animations/bubble-bg.json",
-});
+// var bubble = bodymovin.loadAnimation({
+//   container: document.getElementById("s-bg"),
+//   renderer: "svg",
+//   loop: true,
+//   autoplay: true,
+//   path: "./animations/bubble-bg.json",
+// });
 
 /*HOVER*/
 /*0*/
@@ -133,13 +137,13 @@ btn.addEventListener("mouseenter", () => {
   animation.setDirection(1);
   // animation4.play();
   animation.goToAndPlay(0, true);
-  animation.setSpeed(2);
-});
-btn.addEventListener("mouseleave", () => {
-  animation.setDirection(-1);
-  animation.play();
   animation.setSpeed(1);
 });
+// btn.addEventListener("mouseleave", () => {
+//   animation.setDirection(-1);
+//   animation.play();
+//   animation.setSpeed(1);
+// });
 /*1*/
 btn1.addEventListener("mouseenter", () => {
   animation1.setDirection(1);
@@ -147,11 +151,11 @@ btn1.addEventListener("mouseenter", () => {
   animation1.goToAndPlay(0, true);
   animation1.setSpeed(2);
 });
-btn1.addEventListener("mouseleave", () => {
-  animation1.setDirection(-1);
-  animation1.play();
-  animation1.setSpeed(1);
-});
+// btn1.addEventListener("mouseleave", () => {
+//   animation1.setDirection(-1);
+//   animation1.play();
+//   animation1.setSpeed(1);
+// });
 /*2*/
 btn2.addEventListener("mouseenter", () => {
   animation2.setDirection(1);
@@ -159,11 +163,11 @@ btn2.addEventListener("mouseenter", () => {
   animation2.goToAndPlay(0, true);
   animation2.setSpeed(2);
 });
-btn2.addEventListener("mouseleave", () => {
-  animation2.setDirection(-1);
-  animation2.play();
-  animation2.setSpeed(1);
-});
+// btn2.addEventListener("mouseleave", () => {
+//   animation2.setDirection(-1);
+//   animation2.play();
+//   animation2.setSpeed(1);
+// });
 /*3*/
 btn3.addEventListener("mouseenter", () => {
   animation3.setDirection(1);
